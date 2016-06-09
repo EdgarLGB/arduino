5cb886653569e8b0af0cb880902fea6bafab4f83
EdgarLGB <lgb_edgar@outlook.com>
2016-06-09 22:17:57 +0200

    update android

--- A/app/src/main/java/com/example/edgar/res/MainActivity.java
+++ A/app/src/main/java/com/example/edgar/res/MainActivity.java
@@ -1,4 +1,4 @@
-package com.example.edgar.a;
+package com.example.edgar.res;
 
 import android.media.MediaPlayer;
 import android.os.CountDownTimer;
@@ -7,6 +7,8 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+
+import com.example.edgar.a.R;
 
 import java.io.BufferedWriter;
 import java.io.IOException;

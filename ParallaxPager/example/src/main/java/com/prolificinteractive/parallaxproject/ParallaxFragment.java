78d934d047eda5cdf7e6aac7087432252118b595
EdgarLGB <lgb_edgar@outlook.com>
2016-06-19 23:43:59 +0200

    add titie4

--- ParallaxPager/example/src/main/java/com/prolificinteractive/parallaxproject/ParallaxFragment.java
+++ ParallaxPager/example/src/main/java/com/prolificinteractive/parallaxproject/ParallaxFragment.java
@@ -7,9 +7,12 @@ import android.support.v4.app.Fragment;
 import android.support.v4.view.ViewPager;
 import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 import com.prolificinteractive.parallaxpager.ParallaxContainer;
 
@@ -22,15 +25,15 @@ import java.net.Socket;
 import java.net.UnknownHostException;
 import java.util.concurrent.Executors;
 
-public class ParallaxFragment extends Fragment implements ViewPager.OnPageChangeListener, View.OnClickListener {
+public class ParallaxFragment extends Fragment implements ViewPager.OnPageChangeListener, View.OnTouchListener {
 
   private MediaPlayer player;
   private Socket socket;
   private PrintWriter writer;
-  private static final String SERVER_IP = "192.168.4.1";
-  private static final int PORT = 80;
   private int counter = 0;
   private CountDownTimer timer;
+  private static final String SERVER_IP = "192.168.4.1";
+  private static final int PORT = 80;
   private static final int PERIOD = 2;
   private static final String FAN = "fan";
   private static final String SON = "son";
@@ -56,13 +59,21 @@ public class ParallaxFragment extends Fragment implements ViewPager.OnPageChange
         R.layout.parallax_view_2,
         R.layout.parallax_view_3);
 
-    parallaxContainer.findViewById(R.id.button_fan1).setOnClickListener(this);
+/*    parallaxContainer.findViewById(R.id.button_fan1).setOnClickListener(this);
     parallaxContainer.findViewById(R.id.button_fan2).setOnClickListener(this);
     parallaxContainer.findViewById(R.id.button_fan3).setOnClickListener(this);
 
     parallaxContainer.findViewById(R.id.button_son1).setOnClickListener(this);
     parallaxContainer.findViewById(R.id.button_son2).setOnClickListener(this);
-    parallaxContainer.findViewById(R.id.button_son3).setOnClickListener(this);
+    parallaxContainer.findViewById(R.id.button_son3).setOnClickListener(this);*/
+
+    parallaxContainer.findViewById(R.id.button_fan1).setOnTouchListener(this);
+    parallaxContainer.findViewById(R.id.button_fan2).setOnTouchListener(this);
+    parallaxContainer.findViewById(R.id.button_fan3).setOnTouchListener(this);
+
+    parallaxContainer.findViewById(R.id.button_son1).setOnTouchListener(this);
+    parallaxContainer.findViewById(R.id.button_son2).setOnTouchListener(this);
+    parallaxContainer.findViewById(R.id.button_son3).setOnTouchListener(this);
 
     // optionally set a ViewPager.OnPageChangeListener
     parallaxContainer.setOnPageChangeListener(this);
@@ -87,7 +98,6 @@ public class ParallaxFragment extends Fragment implements ViewPager.OnPageChange
   }
 
   @Override public void onPageSelected(int position) {
-    Toast.makeText(getContext(), "Location: " + position, Toast.LENGTH_SHORT).show();
     player.release();
     switch (position % 3) {
       case 0:
@@ -136,48 +146,75 @@ public class ParallaxFragment extends Fragment implements ViewPager.OnPageChange
     super.onStop();
   }
 
+
   @Override
-  public void onClick(View v) {
-    Log.i("tag", "tag=" + v.getTag());
-    switch ((String)v.getTag()) {
-      case FAN:
-        if (counter == 0) {
-          //start the chrono
-          timer = new CountDownTimer(PERIOD * 1000, 1000) {
-            @Override
-            public void onTick(long millisUntilFinished) {
-
-            }
-
-            @Override
-            public void onFinish() {
-              if (writer == null) {
-                Toast.makeText(getContext(), "Fail to start the fan " + counter, Toast.LENGTH_SHORT).show();
-                counter = 0;
-                return;
-              }
-              if (counter > 0 && counter < 4) {
-                writer.println(counter);
-                Toast.makeText(getContext(), "start the fan " + counter, Toast.LENGTH_SHORT).show();
-                Log.d("info", "fan " + counter + " start");
-              }
-              //reset the counter to 0
-              counter = 0;
-            }
-          };
-          timer.start();
-        }
-        counter++;
-        break;
-      case SON:
-        if (player.isPlaying()) {
-          player.pause();
-          player.seekTo(0);
-        } else {
-          player.start();
+  public boolean onTouch(View v, MotionEvent event) {
+    switch (event.getAction()) {
+      case MotionEvent.ACTION_DOWN: {
+        switch ((String)v.getTag()) {
+          case FAN:
+            v.setBackgroundResource(R.drawable.bizi2);
+            playFan();
+            break;
+          case SON:
+            v.setBackgroundResource(R.drawable.erduo2);
+            playMusic();
+            break;
         }
         break;
+      }
+      case MotionEvent.ACTION_UP: {
+        switch ((String)v.getTag()) {
+          case FAN:
+            v.setBackgroundResource(R.drawable.bizi);
+            break;
+          case SON:
+            v.setBackgroundResource(R.drawable.erduo);
+            break;
+        }
+        break;
+      }
     }
+    return true;
+  }
+
+  private void playMusic() {
+    if (player.isPlaying()) {
+      player.pause();
+      player.seekTo(0);
+    } else {
+      player.start();
+    }
+  }
+
+  private void playFan() {
+    if (counter == 0) {
+      //start the chrono
+      timer = new CountDownTimer(PERIOD * 1000, 1000) {
+        @Override
+        public void onTick(long millisUntilFinished) {
+
+        }
+
+        @Override
+        public void onFinish() {
+          if (writer == null) {
+            Toast.makeText(getContext(), "Fail to start the fan " + counter, Toast.LENGTH_SHORT).show();
+            counter = 0;
+            return;
+          }
+          if (counter > 0 && counter < 4) {
+            writer.println(counter);
+            Toast.makeText(getContext(), "start the fan " + counter, Toast.LENGTH_SHORT).show();
+            Log.d("info", "fan " + counter + " start");
+          }
+          //reset the counter to 0
+          counter = 0;
+        }
+      };
+      timer.start();
+    }
+    counter++;
   }
 
 
